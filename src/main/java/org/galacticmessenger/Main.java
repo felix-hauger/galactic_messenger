b83099ad5437ba9e01dc23803d12320c9c27651f
lucas-ribard <lucas.ribard@laplateforme.io>
2023-10-19 10:19:49 +0200

    ADDED: simple communication between client and server

--- src/main/java/org/galacticmessenger/Main.java
+++ src/main/java/org/galacticmessenger/Main.java
@@ -1,19 +1,25 @@
 package org.galacticmessenger;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException {
+        Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
+        client client = new client();
+        client.startConnection("127.0.0.1", 6666);
+        while (true) {
+            String textInput = myScanner.nextLine();  // Read user input
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            if (textInput.equals("exit")) {
+                String response = client.sendMessage(textInput);
+                client.stopConnection();
+                break;
+            }
+            String response = client.sendMessage(textInput);
+            System.out.println(response);
         }
+
+
     }
 }
